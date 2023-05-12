19dfd3ce19829380354753743a255040f88677f9
Sujan Miah <sujanmiah28@gmail.com>
2023-05-12 20:07:12 +0600

    This is a new commit for what I originally planned to be amended

---
+++ index.js
@@ -0,0 +1,11 @@
+var num = new Array();
+var sum=0;
+
+for(var i=0;i<5;i++){
+    num[i]=parseInt(prompt("enter your number:"))
+    sum=sum+num[i];
+console.log(num[i]);
+
+    
+}
+console.log("sum="+sum);
